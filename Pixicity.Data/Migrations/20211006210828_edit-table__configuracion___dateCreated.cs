eebf6c9b6ab54e452bccbca09b52b677b6774134
Julio Poveda <jjpoveda92@gmail.com>
2021-10-06 16:08:51 -0500

    Migración para actualizar entidad de Configuración

---
+++ Pixicity.Data/Migrations/20211006210828_edit-table__configuracion___dateCreated.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pixicity.Data.Migrations
+{
+    public partial class edittable__configuracion___dateCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                schema: "Web",
+                table: "Configuracion",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                schema: "Web",
+                table: "Configuracion");
+        }
+    }
+}
