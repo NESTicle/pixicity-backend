42d60f8402d87ac19bc5c33cb15ba52c9a465788
nesticle8bit <jjpoveda92@gmail.com>
2022-02-11 11:39:37 -0500

    Puntos en los posts top

--- Pixicity.Domain/ViewModels/Web/TopPostsViewModel.cs
+++ Pixicity.Domain/ViewModels/Web/TopPostsViewModel.cs
@@ -7,6 +7,7 @@ namespace Pixicity.Domain.ViewModels.Web
         public long Id { get; set; }
         public string URL { get; set; }
         public string Titulo { get; set; }
+        public int Puntos { get; set; }
 
         public CategoriaViewModel Categoria { get; set; }
     }
